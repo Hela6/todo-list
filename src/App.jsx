ec348e35510f2d6c432089fadb249e47da7153d6
Leah <leah.jl@proton.me>
2023-06-16 11:18:17 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react"
+import "./App.css"
+import PropTypes from "prop-types";
+import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
+
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos]);
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      const newTodo = {
+        id: Date.now(),
+        title,
+        completed: false
+      };
+
+      return [...currentTodos, newTodo]
+    })
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id != id)
+    })
+  }
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  )
+}
+
+App.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
+    })
+  )
+};
